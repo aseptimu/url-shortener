edaccf9edf510c26cc9d37b364d55b48bc3c7e54
Aleksandr Maslenkov <aamaslenkov@avito.ru>
2025-02-09 11:35:26 +0300

    config fixed

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -11,5 +11,5 @@ var Config ConfigType
 
 func init() {
 	flag.StringVar(&Config.ServerAddress, "a", "localhost:8080", "HTTP server address")
-	flag.StringVar(&Config.BaseAddress, "b", "http://localhost:8080/", "shorten URL base address")
+	flag.StringVar(&Config.BaseAddress, "b", "http://localhost:8080", "shorten URL base address")
 }
